243c182f6dde36c755b10e25b133f721287cc086
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2022-11-22 16:30:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+// const { reporters } = require("mocha")
+
 // code your solution here
+function saturdayFun(a, b='roller-skate'){
+    if (a){
+return "This Saturday, I want to " + a+"!"
+}else {
+    return "This Saturday, I want to " + b+"!"
+}
+}
+function mondayWork(a, b="go to the office"){
+    if (a){
+        return "This Monday, I will " + a+"."
+    }
+    else{
+        return "This Monday, I will " + b+"."
+    }
+}
+
+function wrapAdjective(a ="*"){
+    return function(b ="inner"){
+        return `You are ${a}${b}${a}!`
+
+}}
